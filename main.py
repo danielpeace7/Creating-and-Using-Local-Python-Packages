3c0a451e6d651604ce7afb22eaf633659ce26312
Olayanju Daniel <102909682+danielpeace7@users.noreply.github.com>
2022-06-24 04:11:51 +0100

    Updated the file

--- main.py
+++ main.py
@@ -1,38 +1,88 @@
 import random
 
+Player_Wins = 0
+Computer_Wins = 0
+
+def userOption():
+    userAction = input("Enter a choice (Rock, Paper, Scissors): ")
+    if userAction in ('Rock', 'rock', 'R', 'r'):
+        userAction = 'R'
+
+    elif userAction in ('Paer', 'paper', 'P', 'p'):
+        userAction = 'P'
+
+    elif userAction in ('Scissors', 'scissors', 'S', 's'):
+        userAction = 'S'
+
+    else:
+        print('Invalid Input')
+        userOption()
+    return userAction
+
+def computerChoice():
+    computerAction = random.randint(1,3)
+    if computerAction == 1:
+        computerAction = 'R'
+
+    elif computerAction == 2:
+        computerAction = 'P'
+
+    else:
+        computerAction = 'S'
+
+    return computerAction
+
 while True:
-    userAction = input("Enter a choice (R, P, S): ")
+    print ('')
+    userAction = userOption()
+    computerAction = computerChoice()
+    print ('')
 
-    try:
-        userAction = input()
+    if userAction == 'R':
+        if computerAction == 'R':
+            print (f"Both players selected {userAction}. It's a TIE!")
 
-    except ValueError:
-        print (f"Invalid Input")
-        continue
+        elif computerAction == 'P':
+            print ('Paper covers Rock! You LOSE!')
+            Computer_Wins += 1
 
-    possibleActions = ["R", "P", "S"]
-    computerAction = random.choice(possibleActions)
+        elif computerAction == 'S':
+            print ('Rock smashes Scissors! You WIN!')
+            Player_Wins += 1
 
-    print(f"\nYou chose {userAction}, computer chose {computerAction}.\n")
+    elif userAction == 'P':
+        if computerAction == 'P':
+            print (f"Both players selected {userAction}. It's a TIE!")
 
-    if userAction == computerAction:
-        print(f"Both players selected {userAction}. It's a tie!")
-    elif userAction == "R":
-        if computerAction == "S":
-            print("Rock smashes Scissors! You win!")
-        else:
-            print("Paper covers Rock! You lose.")
-    elif userAction == "P":
-        if computerAction == "R":
-            print("Paper covers Rock! You win!")
-        else:
-            print("Scissors cuts Paper! You lose.")
-    elif userAction == "S":
-        if computerAction == "P":
-            print("Scissors cuts Paper! You win!")
-        else:
-            print("Rock smashes Scissors! You lose.")
+        elif computerAction == 'S':
+            print ('Scissors cuts Paper! You LOSE!')
+            Computer_Wins += 1
 
-    play_again = input("Play again? (Y/N): ")
-    if play_again.lower() != "y":
+        elif computerAction == 'R':
+            print ('Paper covers Rock! You WIN!')
+            Player_Wins += 1
+
+    elif userAction == 'S':
+        if computerAction == 'S':
+            print (f"Both players selected {userAction}. It's a TIE!")
+
+        elif computerAction == 'R':
+            print ('Rock smashes Scissors! You LOSE!')
+            Computer_Wins += 1
+
+        elif computerAction == 'P':
+            print ('Scissors cuts Paper! You WIN!')
+            Player_Wins += 1
+
+    print ('')
+    print ('Player Wins: ' + str(Player_Wins))
+    print ('Computer Wins: ' + str(Computer_Wins))
+    print ('')
+
+    play_again = input("Play again? (y/n): ")
+    if play_again in ['Yes', 'yes', 'Y', 'y']:
+        pass
+    elif play_again in ['No', 'no', 'N', 'n']:
+        break
+    else:
         break
